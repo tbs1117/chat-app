c73e12c3d6edd546d36f1ce433bb77db3f8c9497
tbs1117 <tbs19941117@gmail.com>
2021-08-07 12:08:36 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-
   root to: "messages#index"
-  # get 'messages/index'
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
