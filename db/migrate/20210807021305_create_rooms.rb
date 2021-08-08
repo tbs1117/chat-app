6047fd39763263679ae2e6265cceefa5f8297065
tbs1117 <tbs19941117@gmail.com>
2021-08-08 16:42:45 +0900

    プルダウンの実装

--- db/migrate/20210807021305_create_rooms.rb
+++ db/migrate/20210807021305_create_rooms.rb
@@ -1,8 +1,7 @@
-class CreateRoomUsers < ActiveRecord::Migration[6.0]
+class CreateRooms < ActiveRecord::Migration[6.0]
   def change
-    create_table :room_users do |t|
-      t.references :room, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
+    create_table :rooms do |t|
+      t.string :name, null: false
       t.timestamps
     end
   end
